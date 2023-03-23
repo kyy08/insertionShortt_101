7116eca7c48689df4b1dbec97971fbaf45d0f5ec
kyy08 <kyyelzz1@gmail.com>
2023-03-23 19:05:06 +0700

    procedure input

--- insertionShortt/insertionShortt.cpp
+++ insertionShortt/insertionShortt.cpp
@@ -3,3 +3,20 @@ using namespace std;
 
 int arr[20];	// Membuat array dengan panjang data 20
 int n;			// Membuat variable inputan n
+void input()
+{	// Procedure Input
+	while (true)
+	{
+		cout << "Masukkan Jumlah Data pada Array : ";	// Membuat inputan jumlah element array
+		cin >> n;										// Memanggil variabel inputan n 
+
+		if (n <= 20)
+		{	// Membuat kondisi n tidak lebih dari 20
+			break;
+
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen. \n";
+		}
+	}
