dee78892296b5596657bb8ce2da5ad147b7913fa
kyy08 <kyyelzz1@gmail.com>
2023-03-23 19:07:43 +0700

    menyimpan data pada array

--- insertionShortt/insertionShortt.cpp
+++ insertionShortt/insertionShortt.cpp
@@ -20,3 +20,16 @@ void input()
 			cout << "\nArray yang anda masukkan maksimal 20 elemen. \n";
 		}
 	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+	// Menggunakan perulangan for untuk menyimpan data pada array
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+
+	}
+}
