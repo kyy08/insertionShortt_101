9a034bb43ce0aaa5daf35b513241ecb85c31dfbd
kyy08 <kyyelzz1@gmail.com>
2023-03-23 19:12:00 +0700

    setting nilai j

--- insertionShortt/insertionShortt.cpp
+++ insertionShortt/insertionShortt.cpp
@@ -42,3 +42,4 @@ void insertionsort()
 	for (int i = 1; i < n; i++)		// 1.Membuat looping dengan i dimulai dari 1 hingga n-1
 	{
 		temp = arr[i];	// 2. Simpan nilai arr[i] ke variabel sementara temp
+		j = i - 1;	// 3. Setting nilai j sama dengan i-1;
